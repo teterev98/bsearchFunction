8788e489fcdeb3cd04bb0429312d46353e6d1d8a
teterev98 <39381399+teterev98@users.noreply.github.com>
2018-10-29 16:23:35 +0300

    Update bsearchFunctionт.java

--- bsearchFunctionт.java
+++ bsearchFunctionт.java
@@ -1,13 +1,14 @@
 public static int bsearch(int[] array, int number) {
-    if (array != null && array.length  0 && array[array.length - 1]  number) {   // Проверяем, существует ли решение
+    if (array != null && array.length > 0 && array[array.length - 1] < number) {  // Проверяем, существует ли решение
         return bsearch(array, number, 0, array.length);
     } else return -1;
 }
 
 public static int bsearch(int[] array, int number, int first, int last) {
-    if (last  first) {
-        int midle = (last + first)/2;
-        if (array[midle] = number) {
+
+    if (last > first) {
+        int midle = (last + first) / 2;
+        if (array[midle] >= number) {
             return bsearch(array, number, midle + 1, last);
         } else {
             return bsearch(array, number, first, midle);
